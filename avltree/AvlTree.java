3f98f1e836c9b15b6892dd04e540480df149263c
Michael Borgwardt <brazzy@gmail.com>
2010-05-31 22:53:26 +0200

    Refactored to eliminate null tests

--- avltree/AvlTree.java
+++ avltree/AvlTree.java
@@ -9,6 +9,256 @@ import java.util.Iterator;
  * @author Michael Borgwardt
  */
 public class AvlTree implements Iterable<Long>{
+
+    TreeNode root = new TreeNode(null);
+
+    @Override
+    public Iterator<Long> iterator(){
+        return root.iterator();
+    }
+
+    public void clear(){
+        root = null;
+    }
+
+    public boolean isEmpty(){
+        return root.isEmpty();
+    }
+
+    /**
+     * Adds value to tree
+     *
+     * @param val to add
+     * @return true if value was already present
+     */
+    public boolean insert(long val){
+	return root.insert(val);
+    }
+
+    /**
+     * @return true if val is present in tree
+     */
+    public boolean contains(long val){
+	return root.contains(val);
+    }
+
+    /**
+     * Removes value from tree
+     * @param val to remove
+     * @return true if val was present in tree
+     */
+    public boolean remove(long val){
+	return root.remove(val);
+    }
+
+    @Override
+    public String toString() {
+	return root.toString();
+    }
+
+    /**
+     * Basic (empty) node
+     *
+     * @author Michael Borgwardt
+     */
+    private class TreeNode{
+        /** Parent node */
+        protected ContentNode parent;
+
+        /** Creates empty tree */
+        public TreeNode(ContentNode parent){
+            this.parent = parent;
+        }
+
+        public boolean isEmpty(){
+            return true;
+        }
+
+        public Iterator<Long> iterator(){
+            return new Iterator<Long>(){
+		@Override
+		public boolean hasNext() {
+		    return false;
+		}
+		@Override
+		public Long next() {
+		    throw new UnsupportedOperationException();
+		}
+		@Override
+		public void remove() {
+		    throw new UnsupportedOperationException();
+		}
+            };
+        }
+
+        public boolean insert(long val){
+            ContentNode newNode = new ContentNode(parent, val);
+            if(parent == null){
+        	AvlTree.this.root = newNode;
+            } else if(parent.left == this){
+        	parent.left = newNode;
+            } else{
+        	parent.right = newNode;
+            }
+            return false;
+        }
+
+        public boolean contains(long val){
+            return false;
+        }
+
+        public boolean remove(long val){
+            return false;
+        }
+    }
+
+    /**
+     * Node with content.
+     *
+     * @author Michael Borgwardt
+     */
+    private class ContentNode extends TreeNode{
+        /**
+         * Value contained in this node.
+         */
+        private Long value;
+
+        /** Left subtree */
+        private TreeNode left;
+
+        /** Right subtree */
+        private TreeNode right;
+
+        private ContentNode(ContentNode parent, Long value){
+            super(parent);
+            this.value = value;
+            this.left = new TreeNode(this);
+            this.right = new TreeNode(this);
+        }
+
+        @Override
+        public boolean isEmpty(){
+            return value == null;
+        }
+
+        @Override
+        public Iterator<Long> iterator(){
+            return new TreeIterator(this);
+        }
+
+        private boolean isLeaf(){
+            return left.isEmpty() && right.isEmpty();
+        }
+
+        @Override
+        public boolean insert(long val){
+            if(value == null){
+                this.value = val;
+                return false;
+            } else if(this.value == val){
+                return true;
+            } else if(val < this.value){
+                return left.insert(val);
+            } else {
+                return right.insert(val);
+            }
+        }
+
+        @Override
+        public boolean contains(long val){
+            if(value == null){
+                return false;
+            } else if(this.value == val){
+                return true;
+            } else if (val < this.value) {
+                return left.contains(val);
+            } else {
+                return right.contains(val);
+            }
+        }
+
+        private void replace(TreeNode child, TreeNode replacement){
+            replacement.parent = this;
+            if(child == left){
+        	left = replacement;
+            } else {
+        	right = replacement;
+            }
+        }
+
+        @Override
+        public boolean remove(long val){
+            if(this.value == null){
+                return false;
+            }
+
+            if(this.value == val){
+                if(isLeaf()){
+                    TreeNode empty = new TreeNode(null);
+                    if(parent == null){
+                	AvlTree.this.root = empty;
+                    } else {
+                        parent.replace(this, empty);
+                    }
+                } else if(left == null){
+                    parent.replace(this, right);
+                } else if(right == null){
+                    parent.replace(this, left);
+                } else{
+                    ContentNode pre = predecessor();
+                    Long preVal = pre.value;
+                    pre.remove(preVal);
+                    this.value = preVal;
+                }
+                return true;
+            } else if (val < this.value) {
+                return left.remove(val);
+            } else {
+                return right.remove(val);
+            }
+        }
+
+
+        private ContentNode predecessor(){
+            ContentNode result = (ContentNode)left;
+            while(!result.right.isEmpty()){
+                result = (ContentNode)result.right;
+            }
+            return result;
+        }
+
+        @Override
+        public String toString(){
+            return toString("");
+        }
+
+        private String toString(String prefix){
+            StringBuffer buf = new StringBuffer();
+            buf.append('(');
+            buf.append(value);
+            buf.append(')');
+            buf.append('\n');
+            if(!right.isEmpty())
+            {
+                buf.append(prefix);
+                buf.append("|-");
+                if(left.isEmpty())
+                {
+                    buf.append(((ContentNode)right).toString(prefix+"   "));
+                } else {
+                    buf.append(((ContentNode)right).toString(prefix+"|  "));
+                }
+            }
+            if(!left.isEmpty())
+            {
+                buf.append(prefix);
+                buf.append("|-");
+                buf.append(((ContentNode)left).toString(prefix+"   "));
+            }
+            return buf.toString();
+        }
+    }
+
     /**
      * Iterates over the tree's contents
      *
@@ -18,14 +268,14 @@ public class AvlTree implements Iterable<Long>{
         /** Direction in which to iterate next */
         private static enum Direction{ LEFT, RIGHT, UP};
 
-        private TreeIterator(AvlTree node){
+        private TreeIterator(ContentNode node){
             this.node = node;
             this.dir = Direction.LEFT;
             next();
         }
 
         /** Current node, contains value that will be returned next. */
-        private AvlTree node;
+        private ContentNode node;
 
         /** Direction in which to iterate next */
         private Direction dir;
@@ -42,11 +292,11 @@ public class AvlTree implements Iterable<Long>{
                 traverseLeft();
                 break;
             case RIGHT:
-                node = node.right;
+                node = (ContentNode)node.right;
                 traverseLeft();
                 break;
             case UP:
-                AvlTree previous;
+                ContentNode previous;
                 do{
                     previous = node;
                     node = node.parent;
@@ -65,10 +315,10 @@ public class AvlTree implements Iterable<Long>{
         }
 
         private void traverseLeft(){
-            while(node.left != null){
-                node = node.left;
+            while(!node.left.isEmpty()){
+                node = (ContentNode)node.left;
             }
-            if(node.right != null){
+            if(!node.right.isEmpty()){
                 dir = Direction.RIGHT;
             } else {
                 dir = Direction.UP;
@@ -79,191 +329,5 @@ public class AvlTree implements Iterable<Long>{
         public void remove(){
             throw new UnsupportedOperationException();
         }
-
-    }
-
-    /**
-     * Value contained in this node. Can be null, but only at
-     * the root of the empty tree.
-     */
-    private Long value;
-
-    /** Left subtree */
-    private AvlTree left;
-
-    /** Right subtree */
-    private AvlTree right;
-
-    /** Parent node */
-    private AvlTree parent;
-
-    /** Creates empty tree */
-    public AvlTree(){
-        this(null, null);
-    }
-    private AvlTree(AvlTree parent, Long value){
-        this.parent = parent;
-        this.value = value;
-    }
-
-    private void ensureLeft(){
-        if(this.left == null){
-            this.left = new AvlTree(this, null);
-        }
-    }
-
-    private void ensureRight(){
-        if(this.right == null){
-            this.right = new AvlTree(this, null);
-        }
-    }
-
-    public boolean isEmpty(){
-        return value == null;
-    }
-
-    public void clear(){
-        value = null;
-        left = null;
-        right = null;
-    }
-
-    public Iterator<Long> iterator(){
-        return new TreeIterator(this);
-    }
-
-    private boolean isLeaf(){
-        return left == null && right == null;
-    }
-
-    /**
-     * Adds value to tree
-     *
-     * @param val to add
-     * @return true if value was already present
-     */
-    public boolean insert(long val){
-        if(value == null){
-            this.value = val;
-            return false;
-        } else if(this.value == val){
-            return true;
-        } else if(val < this.value){
-            ensureLeft();
-            return left.insert(val);
-        } else {
-            ensureRight();
-            return right.insert(val);
-        }
-    }
-
-    /**
-     * @return true if val is present in tree
-     */
-    public boolean contains(long val){
-        if(value == null){
-            return false;
-        } else if(this.value == val){
-            return true;
-        } else if (val < this.value) {
-            return left == null ? false : left.contains(val);
-        } else {
-            return right == null ? false : right.contains(val);
-        }
-    }
-
-    private void pullUp(AvlTree child){
-        assert child == left || child == right;
-        value = child.value;
-        left = child.left;
-        right = child.right;
-    }
-
-    /**
-     * Removes value from tree
-     * @param val to remove
-     * @return true if val was present in tree
-     */
-    public boolean remove(long val){
-        if(this.value == null){
-            return false;
-        }
-
-        if(this.value == val){
-            if(isLeaf()){
-                if(parent == null){
-                    value = null;
-                } else if(parent.left == this){
-                    parent.left = null;
-                } else {
-                    parent.right = null;
-                }
-            } else if(left == null){
-                pullUp(right);
-            } else if(right == null){
-                pullUp(left);
-            } else{
-                AvlTree pre = predecessor();
-                Long preVal = pre.value;
-                remove(pre.value);
-                this.value = preVal;
-            }
-            return true;
-        } else if (val < this.value) {
-            if(left == null){
-                return false;
-            } else{
-                return left.remove(val);
-            }
-        } else {
-            if(right == null){
-                return false;
-            } else{
-                return right.remove(val);
-            }
-        }
-    }
-
-    @Override
-    public String toString(){
-        return toString("");
-    }
-
-    private String toString(String prefix){
-        StringBuffer buf = new StringBuffer();
-        buf.append('(');
-        buf.append(value);
-        buf.append(')');
-        buf.append('\n');
-        if(right != null)
-        {
-            buf.append(prefix);
-            buf.append("|-");
-            if(left == null)
-            {
-                buf.append(right.toString(prefix+"   "));
-            } else {
-                buf.append(right.toString(prefix+"|  "));
-            }
-        }
-        if(left != null)
-        {
-            buf.append(prefix);
-            buf.append("|-");
-            buf.append(left.toString(prefix+"   "));
-        }
-        return buf.toString();
-    }
-
-    private AvlTree predecessor(){
-        AvlTree result = left;
-        while(result.right != null){
-            result = result.right;
-        }
-        return result;
-    }
-
-    public long getValue(){
-        return value;
     }
 }
